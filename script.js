7580a2a636ac9e4d25a3255220361b1948d8f52a
Ted-Dev-1 <ted.workday@gmail.com>
2025-06-20 18:42:22 +0000

    Changes for a preview

--- script.js
+++ script.js
@@ -28,9 +28,43 @@ function addTask(){
     saveContent();
 }
 
+input.addEventListener("keydown", function (e) {
+    if (e.key === "Enter") {
+        
+        if(input.value ===""){
+        //alert("Write a task to do. You must write something!");
+            modal.style.display = "flex";
+        }
+
+        else{
+            let listItem = document.createElement("li");
+            listItem.innerText = input.value;
+            list.appendChild(listItem);
+
+            let close = document.createElement("span");
+            close.innerHTML="\u00d7";
+
+            listItem.appendChild(close);
+
+            input.value ="";
+
+            
+        }
+
+        saveContent();
+    }
+});
+
 
 okBtn.addEventListener("click", function(){
     modal.style.display = "none";
+});
+
+
+modal.addEventListener("click", function(e){
+    if(e.target === modal){
+        e.target.style.display = "none";
+    }
 });
 
 
